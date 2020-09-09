23554e764ad4d196aefd34680c044957b8984817
Emmanuel <eariasp@unal.edu.co>
2020-09-09 16:07:39 -0500

    se añaden archivos corregidos y otro par de archivos hechos en el desarrollo de la clase 4

--- clase_4_funciones/hola_mundo.cpp
+++ clase_4_funciones/hola_mundo.cpp
@@ -3,12 +3,7 @@
 //const int a = 20 // variable global no modificable, se usa en todo el código pero no se puede modificar (se puede usar pero mjr no)
 //int a= 20 // variable global modificable, se usa en toddo el código pero se puede modificar (no usar)
 
-void print_msg(double a)
-{
-    std::cout<<"Hello world\n";
-    std::cout << "Parametro a: " << a << "\n";
-    std::cout << "Dirección de a en print :" << &a << std::endl;
-}
+void print_msg(double a);
 
 int main(void)
 {
@@ -16,4 +11,12 @@ int main(void)
     std::cout << "Dirección de a en main: " << &a << std::endl; // el & (amperstand) antes de una variable sirve para extraer la dirección de memoria de una variable
     print_msg(a);
     return 0;
+}
+
+//funcion implementacion
+void print_msg(double a)
+{
+    std::cout<<"Hello world\n";
+    std::cout << "Parametro a: " << a << "\n";
+    std::cout << "Dirección de a en print :" << &a << std::endl;
 }
